f19c07f772baee44f37b9f5dac4a291c0f45805b
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2019-07-12 16:40:22 +0100

    Made CLI

--- renal_segmentor.py
+++ renal_segmentor.py
@@ -5,8 +5,9 @@ Created on Fri Jul 12 11:25:34 2019
 @author: Alex Daniel
 """
 
-# %% Import Packages
+# Import Packages
 import os
+import argparse
 import nibabel as nib
 import numpy as np
 from skimage.transform import resize
@@ -15,7 +16,18 @@ from keras import backend as K
 from keras.models import load_model
 
 
-# %% Define Functions
+parser = argparse.ArgumentParser(prog='Renal Segmentor', description='Segment renal MRI images.')
+parser.add_argument('-i', '--input', required=True, dest='in_name',
+                    help='The image you wish to segment, this can be a PAR/REC, nii.gz, nii or hdr/img.')
+parser.add_argument('-b', '--binary', action='store_true', default=False, dest='binary',
+                    help='The mask output will only be 0 or 1. Default: False')
+parser.add_argument('-r', '--raw', action='store_true', default=False, dest='raw',
+                    help='Output the raw data used for the segmentation as a nii.gz. Default: False')
+parser.add_argument('-o', '--output', default=False, dest='out_name',
+                    help='The name you wish to give your output mask. Default: {input name}_mask.nii.gz')
+args = parser.parse_args()
+# Define Functions
+
 
 def dice_coef(y_true, y_pred):
     smooth = 1.0
@@ -62,17 +74,16 @@ def split_path(full_path):
     return directory, base, extension
 
 
-# %% Import data
+# Import data
 
-img_path = './test_data/test_sub_01.PAR'
-directory, base, extension = split_path(img_path)
+directory, base, extension = split_path(args.in_name)
 binary = True
 raw = True
-img = nib.load(img_path, scaling='fp')
+img = nib.load(args.in_name, scaling='fp')
 data = img.get_data()
 data = pre_process_img(data)
 
-# %% Predict mask
+# Predict mask
 
 if 'model' not in locals():
     model = load_model('.\models\All46_norm_0.93008.model',
@@ -81,14 +92,18 @@ batch_size = 2 ** 3
 
 prediction = model.predict(data, batch_size=batch_size)
 mask = un_process_mask(prediction, img)
-if binary:
+if args.binary:
     mask = (mask > 0.5) * 1
 
-# %% Output mask
+# Output mask
 
-output_path = directory + '/' + base + '_mask.nii.gz'
+if args.out_name==False:
+    output_path = directory + '/' + base + '_mask.nii.gz'
+else:
+    output_path = args.out_name
+
 mask_img = nib.Nifti1Image(mask, img.affine)
 nib.save(mask_img, output_path)
 
-if raw:
+if args.raw:
     nib.save(img, directory + '/' + base + '.nii.gz')
