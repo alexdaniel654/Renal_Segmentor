2ffbc2c98b9424b3be845c4ae33662ec83806b32
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2019-09-17 11:20:23 +0100

    Update segmentation application to work with current best model

--- renal_segmentor.py
+++ renal_segmentor.py
@@ -10,7 +10,6 @@ import os
 import nibabel as nib
 import numpy as np
 from skimage.transform import resize
-from sklearn import preprocessing
 from keras import backend as K
 from keras.models import load_model
 import tensorflow as tf
@@ -34,25 +33,33 @@ def dice_coef_loss(y_true, y_pred):
 
 
 def pre_process_img(raw_data):
-    data = np.flipud(raw_data)
-    for n in np.arange(data.shape[2]):
-        data[:, :, n] = preprocessing.normalize(data[:, :, n])
-    data = resize(data, (256, 256, data.shape[2]), preserve_range=True)
-    data = data.reshape((data.shape[0], data.shape[1], data.shape[2], 1))
-    data = np.rot90(data, -1)
-    data = np.swapaxes(data, 0, 2)
+    data = np.swapaxes(raw_data, 0, 2)
     data = np.swapaxes(data, 1, 2)
+    for n in range(data.shape[0]):
+        data[n, :, :] = rescale(data[n, :, :])
+    data = resize(data, (data.shape[0], 256, 256))
+    data = data.reshape((data.shape[0], data.shape[1], data.shape[2], 1))
     return data
 
 
 def un_process_mask(mask, base_img):
-    mask = mask.reshape(mask.shape[0], mask.shape[1], mask.shape[2])
+    mask = np.squeeze(mask)
     mask = np.swapaxes(mask, 0, 2)
     mask = np.swapaxes(mask, 0, 1)
     mask = resize(mask, (base_img.shape[0], base_img.shape[1], base_img.shape[2]))
-    mask = np.rot90(mask)
-    mask = np.flipud(mask)
     return mask
+
+
+def rescale(data):
+    black = np.mean(data) - 0.5 * np.std(data)
+    if black < data.min():
+        black = data.min()
+    white = np.mean(data) + 4 * np.std(data)
+    if white > data.max():
+        white = data.max()
+    data = np.clip(data, black, white)-black
+    data = data/(white-black)
+    return data
 
 
 def split_path(full_path):
@@ -105,7 +112,7 @@ def main():
 
     # Predict mask
     print('Loading model')
-    model = load_model(resource_path('./models/All46_norm_0.93008.model'),
+    model = load_model(resource_path('./models/very_extreme_augmentation_300_epochs_max_dice_0.9262.model'),
                        custom_objects={'dice_coef_loss': dice_coef_loss, 'dice_coef': dice_coef})
 
     print('Making prediction')
